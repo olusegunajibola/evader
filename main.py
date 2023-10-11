fe8f84271b71a1018a992d9626b9e2ba8c230d14
olusegunajibola <olusegunajibola.e@gmail.com>
2023-10-11 20:52:42 +0200

    implement collision 1.0

--- main.py
+++ main.py
@@ -1,37 +1,123 @@
+import math
+
 import pygame
+import random
 
 WHITE = (255, 255, 255)
-
+number_of_obstacles = 4
 # initialize the game
 pygame.init()
 
+#                              (X    ,     Y)
 # create the screen            (width, height)
 screen = pygame.display.set_mode((800, 600))
+
+# background
+background = pygame.image.load("background.png")
 
 # title & icon
 pygame.display.set_caption("Evader")
 icon = pygame.image.load("space-ship.png")
 pygame.display.set_icon(icon)
 
+# obstacle
+obstacle = pygame.image.load("barrier.png")  # 124px
+obstacleX = random.randint(0, 676)
+obstacleY = random.randint(0, 476)
+
+print('obstacleX: ', obstacleX, 'obstacleY: ', obstacleY)
+
+agentX_change = 4  # 0.3
+agentY_change = 4  # 0.3
+
 # Agent
 agent = icon
-agentX = 370  # X & Y are coordinates, i.e. positions
-agentY = 480
+# agentX = 370  # X & Y are coordinates, i.e. positions
+# agentY = 480
+
+agentX = random.randint(1, 800)
+agentY = random.randint(1, 600)
 
 
-def agent_draw():
-    screen.blit(agent, (agentX, agentY))  # means to draw
+def agent_draw(x, y):
+    # screen.blit(agent, (agentX, agentY))  # means to draw
+    screen.blit(agent, (x, y))  # means to draw
+
+
+def obs_draw(x, y):
+    screen.blit(obstacle, (x, y))  # means to draw
+    # for i in range(1, number_of_obstacles):
+    #     screen.blit(obstacle, (x, y))  # means to draw
+
+
+def is_collision(obstacleX, obstacleY, agentX, agentY):
+    distance = math.sqrt((math.pow(agentX - obstacleX, 2))
+                         + (math.pow(agentY - obstacleY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 # game loop
 running = True
 while running:
 
-    screen.fill(WHITE)  # this will not work, an update display is needed
+    # screen.fill(WHITE)  # this will not work, an update display is needed
+    screen.blit(background, (0, 0))
+
+    # agentY -= 0.1
+    agentY += agentY_change
+
+    collision = is_collision(obstacleX, obstacleY, agentX, agentY)
+    if collision:
+        print('before: ', agentX, agentY)
+        agentY -= agentY_change
+        agentX += agentX_change
+        print('after: ', agentX, agentY)
+
+    # agentX -= 0.1
+    agentX += agentX_change
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    agent_draw()
+    # contain within boundary of the game
+    if agentX <= 0:
+        # agentX = 0.5
+        agentX_change = 4
+    elif agentX >= 768:  # 768 is based on the value of the
+        # agent's pixel i.e 32px
+        # agentX = 767.5
+        agentX_change = -4
+
+    # contain within boundary of the game
+    if agentY <= 0:
+        agentY_change = 4
+    elif agentY >= 568:  # 568 is based on the value of the
+        # agent's pixel i.e 32px
+        # agentY = 567.5
+        agentY_change = -4
+
+    # print('agentX: ', agentX, 'agentY: ', agentY)
+
+    #  collision
+    # collision = is_collision(obstacleX, obstacleY, agentX, agentY)
+    # if collision:
+    #     agentY += 0.5
+
+    # for i in range(1, number_of_obstacles):
+    #     obs_draw(obstacleX, obstacleY)
+    obs_draw(obstacleX, obstacleY)
+    # obs_draw(obstacleX, obstacleY)
+
+    agent_draw(agentX, agentY)  # enables the agent to move
+    # for i in range(1, number_of_obstacles):
+    #     obs_draw(obstacleX, obstacleY)
     pygame.display.update()
+
+# TODO 0. insert 3 obstacles.
+# TODO 1. yet to fix the collision of 3 obstacles and evader agent
+# TODO 2. i think preference to go up has been fixed
+# TODO 3. If it hits the boundary, it should bounce back choosing any random direction
